772159b953fa3b0473f911966c55274589b856db
jwong65 <jason.wong6507@gmail.com>
2025-11-01 11:41:11 -0400

    Installed more packages, updated vite config for Tailwind. Added Navbar styling to help test

--- mock/src/App.jsx
+++ mock/src/App.jsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+import "./App.css"
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<div>Home</div>} />
       </Routes>
--- mock/src/components/Navbar.jsx
+++ mock/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Navbar.css"
 
 export default function Navbar() {
   return (
-    <header>
-        Logo
-    </header>
+    <nav className="relative bg-gray-800">
+      <div className="max-w-7xl mx-auto flex items-center justify-between px-4 py-4">
+        <ul className="flex items-center gap-6 text-sm font-medium">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
